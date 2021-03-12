79db60fec180588e1dca01b6ad8c3f7956c1e294
kxdAmmo <fungaip163@outlook.com>
2021-03-12 23:06:52 +0200

    Created Header Component and added its nav links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { Header } from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route component={HomePage} path='/' exact />
-        <Route component={ShopPage} path='/shop' />
+        <Route component={HomePage} path="/" exact />
+        <Route component={ShopPage} path="/shop" exact />
       </Switch>
     </div>
   );
